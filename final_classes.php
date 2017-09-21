056ce63ab63a63acc248d1c19c4d5793f4d55e2d
Ankush <ankush.thakur53@gmail.com>
2017-09-21 18:50:16 +0530

    Modify final classes example to show that objects can still be modified

--- final_classes.php
+++ final_classes.php
@@ -16,3 +16,11 @@ class BClass extends AClass {
 }
 */
 
+// However, the dynamic nature of PHP means that the class definitions aren't preserved.
+// It's always possible to directly modify the objects of a final class, so this 'safety'
+// is limited to inheritance only
+
+$a = new AClass();
+$a->name = 'Jojo';
+
+var_dump($a);
