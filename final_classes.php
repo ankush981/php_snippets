b93d6de6ca59c97a453f40a99ec30510b2efd28b
Ankush <ankush.thakur53@gmail.com>
2017-09-21 18:48:22 +0530

    Add final_classes example

---
+++ final_classes.php
@@ -0,0 +1,18 @@
+<?php
+
+//T he final keyword is used to mark a class as final
+
+final class AClass {
+
+}
+
+// which prevents it from being subclassed
+
+// -- throws error --
+
+/*
+class BClass extends AClass {
+
+}
+*/
+
