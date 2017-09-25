dcddce5cc8a94cfd30e73f8665f0defdff0cd841
Ankush <ankush.thakur53@gmail.com>
2017-09-25 13:25:42 +0530

    Show usage of the clone keyword

--- cloning_objects.php
+++ cloning_objects.php
@@ -7,4 +7,12 @@ $a->name = 'Ankush';
 
 $b = $a;
 
-echo ($b == $a); // outputs 1
+echo (int)($b === $a); // outputs 1
+echo "\n";
+
+// However, there is a clone keyword that makes a copy of the object
+
+$c = clone $a;
+echo (int)($c === $a); // outputs 0
+echo "\n";
+
