65c240d82290632fe2dc31c2dc77f36919b4e3d6
Ankush <ankush.thakur53@gmail.com>
2017-09-26 18:16:29 +0530

    Add __clone() description to cloning example

--- cloning_objects.php
+++ cloning_objects.php
@@ -18,3 +18,7 @@ echo "\n";
 
 // That said, the 'clone' keyword makes a shallow copy, which means that if some properties of the cloned object are other objects, these will be shared by reference. (ouch?)
 
+// To make a deep copy of an object in PHP, override the default __clone() method
+// make a 'clone' of the objects inside. Of course, if there are more nested objects,
+// you need to keep doing the hard work!
+
