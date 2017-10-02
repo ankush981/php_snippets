98faa0e120a874d45d24d0f33402ed50d79f4c0d
Ankush <ankush.thakur53@gmail.com>
2017-10-02 07:32:35 +0530

    Add class constants example

---
+++ class_constants.php
@@ -0,0 +1,18 @@
+<?php
+
+// There are situations where we might need class-level constants.
+// These are achieved in PHP by using 'const' declarations
+
+class Circle {
+    const pi = 3.14159;
+    private $radius;
+
+    public function __construct($radius) {
+        $this->radius = $radius;
+    }
+
+    public function getArea() {
+        // Notice that 'pi' can be used directly here
+        return $this->radius * $this->radius * pi;
+    }
+}
